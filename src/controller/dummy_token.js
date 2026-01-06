85511f3d007c686ad7e43358325336718d7b24fc
Sanjosunny444 <sanjosunny2001@gmail.com>
2026-01-06 10:49:38 +0530

    add these dummy functions for my medical project

---
+++ src/controller/dummy_token.js
@@ -0,0 +1,80 @@
+// a dummy token for my another project totaly different from this one
+// 2. Device Registration (POST API)
+// Device sends a POST request to the server with:
+// MAC ID (device identification)
+// Wi-Fi signal strength (for debugging)
+// SSID (for debugging)
+// Timestamp (for debugging)
+// Server stores this information and confirms the login/registration status.
+/*
+void registerDevice() {
+    int8_t rssi = WiFi.RSSI();
+    String ssid = WiFi.SSID();
+    StaticJsonDocument<256> doc;
+    doc["macId"] = mac;
+    doc["Rssi"] = rssi;
+    doc["ssid"] = ssid;
+    char buffer[27];
+    getCurrentDateTimeISO8601(buffer);
+    doc["timestamp"] = buffer;
+    char body[256];
+    serializeJson(doc, body);
+    sendHttpRequest(client, server_client, 443, "POST", "/api/register", "application/json", body);
+}
+
+
+void callNextPatient() {
+    StaticJsonDocument<128> doc;
+    doc["currentToken"] = currentToken;
+    doc["macId"] = mac;
+    char buffer[27];
+    getCurrentDateTimeISO8601(buffer);
+    doc["timestamp"] = buffer;
+
+    char body[128];
+    serializeJson(doc, body);
+    sendHttpRequest(client, server_client, 443, "GET", "/api/call-next", nullptr, nullptr);
+}
+
+void recallToken() {
+    StaticJsonDocument<128> doc;
+    doc["currentToken"] = currentToken;
+    doc["macId"] = mac;
+    char buffer[27];
+    getCurrentDateTimeISO8601(buffer);
+    doc["timestamp"] = buffer;
+
+    char body[128];
+    serializeJson(doc, body);
+
+    sendHttpRequest(client, server_client, 443, "POST", "/api/recall", "application/json", body);
+}
+*/
+exports.registerDevice = () => {
+    try {
+        res.status(200).json({ message: 'Device registered successfully' });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+const token_number = 1;
+
+exports.callNextPatient = () => {
+    try {
+        res.status(200).json({ message: 'Patient called successfully', token_number: token_number + 1 });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.recallToken = () => {
+    try {
+        res.status(200).json({ message: 'Token recalled successfully', token_number: token_number});
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+}
--- src/routes/user.Routes.js
+++ src/routes/user.Routes.js
@@ -7,6 +7,7 @@ const { signUp, getAllUsers, logout } = require('../controller/userController');
 const { login } = require('../controller/userController');
 const { verifyToken } = require('../config/isAuth');
 const {verifyrefereshTokeninDB, renew_refresh_token} = require('../controller/tokenController');
+const {registerDevice , callNextPatient , recallToken} = require('../controller/dummy_token');
 const router = express.Router();
 
 router.post('/login', login);
@@ -15,4 +16,7 @@ router.post('/logout', logout);
 router.get("/getall", verifyToken, getAllUsers);
 router.get("/verifyrefreshtoken", verifyrefereshTokeninDB);
 router.post("/renew_refresh_token",renew_refresh_token);
+router.post("/api/register", registerDevice);
+router.get("/api/call-next", callNextPatient);
+router.post("/api/recall-token", recallToken);
 module.exports = router;
